77e2082e472f38a7de49fd33753786f3cfbe13b8
Keenan <Keenanking555@gmail.com>
2018-09-20 12:51:52 -0600

    added the first popup.

--- src/Controller/ifController.java
+++ src/Controller/ifController.java
@@ -1,6 +1,28 @@
 package Controller;
 
+//Import Section
+import Model.Guardian;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class ifController
 {
-
+	private Guardian userGuardian;
+	
+	public ifController()
+	{
+		userGuardian = new Guardian();
+	}
+	
+	/**
+	 * this is where the program starts calling methods to run the program.
+	 */
+	public void start()
+	{
+		Scanner inputGuardian = new Scanner(System.in);
+		String userInput = JOptionPane.showInputDialog(null, "What should it's name be?");
+		userGuardian.setName(userInput);
+	}
 }
--- src/Controller/ifRunner.java
+++ src/Controller/ifRunner.java
@@ -2,5 +2,9 @@ package Controller;
 
 public class ifRunner
 {
-
+	public static void main (String [] args)
+	{
+		ifController app = new ifController();
+		app.start();
+	}
 }
